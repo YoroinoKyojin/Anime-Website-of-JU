75efaa77d56bf5554bba17b3fc1659efa3273c6f
RivuprovoDe <rivuprovode@gmail.com>
2025-02-18 13:14:44 +0530

    Add files via upload

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
-import "./HomePage.css";
+import "./HomePage.css"
+import video from "../Assets/vegeta-ultra-ego.3840x2160.mp4"
 export const HomePage = () => {
   return (
-    <div className='homepage'>HomePage</div>
+    <div>
+      <div class="overlay">
+    <div class="grid-container homepage">
+    <div class="grid-item homepage-1-1">Section 1</div>
+    <div class="grid-item homepage-1-2">Section 2</div>
+    <div class="grid-item homepage-1-3">Section 3</div>
+      </div>
+      </div>
+    <div className='homepage homepage-2'>HomePage</div>
+    <div className="homepage homepage-3">
+        <video src ={video} autoPlay loop muted className="video-bg">
+        </video>
+        <div className="overlay-text">HomePage</div>
+      </div>
+    {/* <div className='homepage homepage-3'>HomePage</div>
+    <div className='homepage homepage-4'>HomePage</div>
+    <div className='homepage homepage-5'>HomePage</div> */}
+    </div>
   )
 }
